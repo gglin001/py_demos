eb824880336703155d9ab80a5d6bc4bee5f41f2b
Allen Guo <guosonglin001@gmail.com>
2024-09-13 22:07:25 +0800

    wip

--- scripts/t1.nogil.sh
+++ scripts/t1.nogil.sh
@@ -2,3 +2,8 @@
 micromamba create -n nogil -c conda-forge/label/python_rc python-freethreading
 
 micromamba activate nogil
+
+# https://py-free-threading.github.io/running-gil-disabled/
+python -VV
+
+# Python 3.13.0rc2 experimental free-threading build | packaged by conda-forge | ...
